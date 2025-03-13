3e087be711d4dc4c900a702164ec883f6a998396
Chirag <chiragnabhoya2506@gmail.com>
2025-03-13 21:48:46 +0100

    Conversion of dataTypes part 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,11 @@
 let score = "Chirag"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -12,3 +13,18 @@ console.log(valueInNumber);
 // true => 1; false => 0
 // null => 0
 // undefined => NaN
+
+let isLoggedIn = "Chirag"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Chirag" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

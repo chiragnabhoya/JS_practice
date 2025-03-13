92d411934504179b8b240b6ae8852a47f8a0fdb8
Chirag <chiragnabhoya2506@gmail.com>
2025-03-13 22:50:55 +0100

    Conversion of dataTypes part 3

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************** Operations *************************************
+
+let value = 33
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3 + 3); //6
+// console.log(3 - 3); //0
+// console.log(3 * 3); //9
+// console.log(3 / 3); //1
+// console.log(3 % 3); //0
+
+let str1 = "Chirag"
+let str2 = " Nabhoya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2"); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3); //7
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);

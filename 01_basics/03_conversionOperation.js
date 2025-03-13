685ef4803d44530ecea0fe1b54cd4f080b1dc65a
Chirag <chiragnabhoya2506@gmail.com>
2025-03-13 21:35:03 +0100

    Conversion of dataTypes part 1

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "Chirag"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN

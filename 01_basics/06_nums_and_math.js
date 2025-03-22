d10df915672f1e6efb34362e014c22e9fbed39f6
Chirag <chiragnabhoya2506@gmail.com>
2025-03-22 23:06:03 +0100

    further math functions

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -15,10 +15,18 @@ const hundreds = 1000000
 
 // ++++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.4));
-console.log(Math.ceil(4.1));
-console.log(Math.floor(4.9));
-console.log(Math.min(4, 5, 6, 7, 8, 9));
-console.log(Math.max(4, 5, 6, 7, 8, 9));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 6, 7, 8, 9));
+// console.log(Math.max(4, 5, 6, 7, 8, 9));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min);

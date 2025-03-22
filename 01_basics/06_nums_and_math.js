8c9290d0a31d9f9a2d3ceffaf5b5bb21cb91a4f8
Chirag <chiragnabhoya2506@gmail.com>
2025-03-22 22:47:10 +0100

    Initial math fuctions

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -15,3 +15,10 @@ const hundreds = 1000000
 
 // ++++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++
 
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 5, 6, 7, 8, 9));
+console.log(Math.max(4, 5, 6, 7, 8, 9));
